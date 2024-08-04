f59d12c328a4e09c4cb0c6cd15cb88f14505c720
ghian-gianan <156592476+ghian-gianan@users.noreply.github.com>
2024-08-04 14:16:44 +0800

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,10 @@
+import statistics
+
+myList = [32, 25, 51, 65, 5, 2, 21]
+
+print("Mean: ", statistics.mean(myList))
+
+print("Median: ", statistics.median(myList))
+
+print("Mode: ", statistics.mode(myList))
+
